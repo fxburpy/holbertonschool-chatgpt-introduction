395035c0cfc9807f78a3bc897cafd8e1c0c97c03
fxburpy <13808@holbertonstudents.com>
2026-07-02 17:36:11 +0400

    Update factorial.py

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -5,9 +5,10 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1
+        n -= 1  # <-- This line fixes the infinite loop
     return result
 
 if __name__ == "__main__":
-    f = factorial(int(sys.argv[1]))
-    print(f)
+    if len(sys.argv) > 1:
+        f = factorial(int(sys.argv[1]))
+        print(f)
